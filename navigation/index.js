b03dd92d31f60486ea2665b0979865528b4f505e
Wai Yan <jellywaiyan@gmail.com>
2023-06-20 00:08:24 +0800

    small changes

--- navigation/index.js
+++ navigation/index.js
@@ -15,21 +15,25 @@ import { StyleSheet } from 'react-native';
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
+    //Auth.signIn("ur username","Put ur pw here");
+    //If u have issues/its stuck on the loading page when u refresh u uncomment,
+    //put in the login details and save and run then comment out the Auth.signin,
+    //refresh and it should work as normal
+    //working on a fix for this
+    const [user, setUser] = useState(undefined);
 
-    // const [user, setUser] = useState(undefined);
+    const checkUser = async () => {
+        try {
+            const authUser = await Auth.currentAuthenticatedUser({bypassCache: true});
+            setUser(authUser); 
+        } catch(e) {
+            setUser(null);
+        }
+    };
 
-    // const checkUser = async () => {
-    //     try {
-    //         const authUser = await Auth.currentAuthenticatedUser({bypassCache: true});
-    //         setUser(authUser); 
-    //     } catch(e) {
-    //         setUser(null);
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     checkUser();
-    // }, [])
+    useEffect(() => {
+        checkUser();
+    }, [])
 
     // useEffect(() => {
     //     const listener = data => {
@@ -39,28 +43,40 @@ const Navigation = () => {
     //     Hub.listen("auth", listener);
     // }, []);
 
-    // if (user == undefined) {
-    //     return (
-    //         <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-    //             <Image 
-    //   style={[styles.logo, {bottom:20}]}
-    //   resizeMode='contain'
-    //   source={require("./../assets/AllezBlocAppLogo.jpg")}/>
-    //   <Text>AllezBloc</Text>
-    //         </View>
-    //     );
-    // };
+    if (user == undefined) {
+        return (
+            <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                <Image 
+      style={[styles.logo, {bottom:20}]}
+      resizeMode='contain'
+      source={require("./../assets/AllezBlocAppLogo.jpg")}/>
+      <Text>AllezBloc</Text>
+            </View>
+        );
+    };
     
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown: false}}>
+                {user ? (          
+                <> 
+                <Stack.Screen name= "Home"component={Home}/>
+                <Stack.Screen name= "Sign In"component={WelcomeScreen}/>
+                <Stack.Screen name= "Sign Up"component={SignUpScreen}/>
+                <Stack.Screen name= "Confirm Email"component={ConfirmEmailScreen}/>
+                <Stack.Screen name= "Forgot Password"component={ForgotPasswordScreen}/>
+                <Stack.Screen name= "Reset Password"component={ResetPasswordScreen}/>
+                </>
+                ) : (
+                <>
             <Stack.Screen name= "Sign In"component={WelcomeScreen}/>
             <Stack.Screen name= "Sign Up"component={SignUpScreen}/>
             <Stack.Screen name= "Confirm Email"component={ConfirmEmailScreen}/>
             <Stack.Screen name= "Forgot Password"component={ForgotPasswordScreen}/>
             <Stack.Screen name= "Reset Password"component={ResetPasswordScreen}/>
-            <Stack.Screen name= "Home"component={Home}/>
             <Stack.Screen name= "Chat"component={ChatScreen}/>
+            </>
+                )}
             </Stack.Navigator>
         </NavigationContainer>
     );
